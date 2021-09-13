5318f4639f736b922709b884ad1910e9cf16a45c
Anastasiya-code <nastyushad@gmail.com>
2021-09-13 12:21:20 +0300

    Update CalculatorMaxArray.java
    Калькулятор + поиск наибольшего элемента в массиве

--- CalculatorMaxArray.java
+++ CalculatorMaxArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Calculator {
+public class CalculatorMaxArray {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
